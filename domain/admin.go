1bd054ae2bfdf2887dd2289b5661bbd66ebe4b75
Pongsakorn <ohm_pongsakorn2@gmail.com>
2024-03-01 00:33:26 +0700

    create list admins function

--- domain/admin.go
+++ domain/admin.go
@@ -4,8 +4,10 @@ import "order-management/entity"
 
 type AdminUsecase interface {
 	CreateAdmin(admin entity.Admin) error
+	ListAdmins() ([]entity.Admin, error)
 }
 
 type AdminRepository interface {
 	CreateAdmin(admin entity.Admin) error
+	ListAdmins() ([]entity.Admin, error)
 }
