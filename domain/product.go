1fb8c6a810c6a304959ebf051591dfd0de610c57
Pongsakorn <ohm_pongsakorn2@gmail.com>
2024-03-01 00:33:40 +0700

    create product domain

--- domain/product.go
+++ domain/product.go
@@ -1,1 +1,11 @@
 package domain
+
+import "order-management/entity"
+
+type ProductUsecase interface {
+	CreateProduct(product entity.Product) error
+}
+
+type ProductRepository interface {
+	CreateProduct(product entity.Product) error
+}
