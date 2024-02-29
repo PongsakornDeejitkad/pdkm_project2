669552a46d2df32f9b460eee03c63d52ae722e80
Pongsakorn <ohm_pongsakorn2@gmail.com>
2024-03-01 00:33:09 +0700

    import product path

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	adminDelivery "order-management/features/admin/delivery/admin"
 	adminRepository "order-management/features/admin/repository"
 	adminUsecase "order-management/features/admin/usecase"
+	productDelivery "order-management/features/product/delivery"
+	productRepository "order-management/features/product/repository"
+	productUsecase "order-management/features/product/usecase"
 	"order-management/utils"
 	"os"
 	"os/signal"
@@ -21,50 +24,6 @@ import (
 
 var runEnv string
 var DB *gorm.DB
-
-func init() {
-	runEnv = os.Getenv("RUN_ENV")
-	if runEnv == "" {
-		runEnv = "dev"
-	}
-
-	utils.InitViper()
-
-	var err error
-	if err = connectDB(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]interface{}{"status": true})
-	})
-
-	v1 := e.Group("/v1")
-
-	adminV1Group := v1.Group("/admin")
-	adminDelivery.NewAdminHandler(adminV1Group, adminUsecase.NewAdminUsecase(adminRepository.NewAdminRepository(DB)))
-
-	serveGracefulShutdown(e)
-}
-
-func connectDB() error {
-	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
-		utils.ViperGetString("postgres.host"),
-		utils.ViperGetString("postgres.user"),
-		utils.ViperGetString("postgres.password"),
-		utils.ViperGetString("postgres.dbname"),
-		utils.ViperGetString("postgres.port"))
-
-	var err error
-	DB, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
-
-	// migrateDB()
-
-	return err
-}
 
 func serveGracefulShutdown(e *echo.Echo) {
 	go func() {
@@ -106,7 +65,53 @@ func migrateDB() {
 		&entity.Admin{},
 		// &AdminType{},
 		// &Sessions{},
-		&entity.User{},
+		// &entity.User{},
 	// &Orders{},
 	)
+}
+
+func init() {
+	runEnv = os.Getenv("RUN_ENV")
+	if runEnv == "" {
+		runEnv = "dev"
+	}
+
+	utils.InitViper()
+
+	var err error
+	if err = connectDB(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{"status": true})
+	})
+
+	v1 := e.Group("/v1")
+
+	adminV1Group := v1.Group("/admins")
+	productV1Group := v1.Group("/product")
+
+	adminDelivery.NewAdminHandler(adminV1Group, adminUsecase.NewAdminUsecase(adminRepository.NewAdminRepository(DB)))
+	productDelivery.NewProductHandler(productV1Group, productUsecase.NewProductUsecase(productRepository.NewProductRepository(DB)))
+	serveGracefulShutdown(e)
+}
+
+func connectDB() error {
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		utils.ViperGetString("postgres.host"),
+		utils.ViperGetString("postgres.user"),
+		utils.ViperGetString("postgres.password"),
+		utils.ViperGetString("postgres.dbname"),
+		utils.ViperGetString("postgres.port"))
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+
+	migrateDB()
+
+	return err
 }
