56a9c468323b364510770a16b71a2bfc635b4976
Pongsakorn <ohm_pongsakorn2@gmail.com>
2024-03-04 15:03:52 +0700

    create validator

--- entity/admin.go
+++ entity/admin.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Admin struct {
-	ID        int            `json:"id" gorm:"primary_key" `
-	Username  string         `json:"user_name" gorm:"not null;varchar(50)"`
-	Password  string         `json:"password" gorm:"not null;type:text;size:200"`
-	FirstName string         `json:"first_name" gorm:"varchar;not null"`
-	LastName  string         `json:"last_name" gorm:"varchar;not null"`
-	Email     string         `json:"email" gorm:"varchar(100);not null;unique"`
+	ID        int            `json:"id" gorm:"primary_key"`
+	Username  string         `json:"user_name" gorm:"not null;varchar(50)" validate:"required"`
+	Password  string         `json:"password" gorm:"not null;type:text;size:200" validate:"required,min=8"`
+	FirstName string         `json:"first_name" gorm:"varchar;not null" validate:"required"`
+	LastName  string         `json:"last_name" gorm:"varchar;not null" validate:"required"`
+	Email     string         `json:"email" gorm:"varchar(100);not null;unique" validate:"required,email"`
 	TypeID    int            `json:"type_id" gorm:"integer"`
-	LastLogin time.Time      `json:"last_login" gorm:"default: NOW()"`
-	CreatedAt time.Time      `json:"created_at" gorm:"default: NOW()"`
+	LastLogin time.Time      `json:"last_login" gorm:"default: NOW()" `
+	CreatedAt time.Time      `json:"created_at" gorm:"default: NOW()" `
 	UpdatedAt time.Time      `json:"updated_at" gorm:"default: NOW()"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 
@@ -24,7 +24,7 @@ type Admin struct {
 
 type AdminType struct {
 	ID         int       `json:"id" gorm:"primary_key"`
-	AdminType  string    `json:"admin_type" gorm:"varchar"`
+	AdminType  string    `json:"admin_type" gorm:"varchar" `
 	Permission string    `json:"permission" gorm:"varchar"`
 	CreatedAt  time.Time `json:"created_at" gorm:"default:NOW()"`
 	UpdatedAt  time.Time `json:"updated_at" gorm:"default:NOW()"`
